d74f5da5f46cd417ead6a9ca82b5a249fc0e83d9
Kai Koh <kaikoh95@gmail.com>
2021-06-27 16:56:41 +1200

    Develop: Corrected attributes for Location Schema.

--- app/cows/schemas/cow_schemas.py
+++ app/cows/schemas/cow_schemas.py
@@ -2,21 +2,20 @@ from marshmallow import Schema, fields, validate
 
 
 class LocationSchema(Schema):
-    collarId = fields.Float(required=True, default=123.45)
-    cowNumber = fields.Float(required=True, default=123.45)
+    long = fields.Float(required=True, example=123.45)
+    lat = fields.Float(required=True, example=123.45)
 
 
 class CowSchema(Schema):
-    id = fields.Str(required=True, example="xxxxxx-xxxx-xxxx-xxxxx")
+    id = fields.String(required=True, example="xxxxxx-xxxx-xxxx-xxxxx")
     collarId = fields.String(required=True, example="50",
-                             validate=validate.OneOf([str(i) for i in range(1, 51)]),
-                             error_messages={"required": {"message": "Between 1 and 50 (inclusive) only", "code": 404}}
-                             )
+                             validate=validate.OneOf([str(i) for i in range(1, 51)]))
     cowNumber = fields.String(required=True, example="123")
     collarStatus = fields.String(required=True, example="123")
     lastLocation = fields.Nested(LocationSchema(), required=True)
 
 
 class CowRequestSchema(Schema):
-    collarId = fields.String(required=True, example="123")
+    collarId = fields.String(required=True, example="23",
+                             validate=validate.OneOf([str(i) for i in range(1, 51)]))
     cowNumber = fields.String(required=True, example="123")
